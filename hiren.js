b8961ed9df178bb1ebe7d08718e9954e6686f222
pyprism <git.pyprism@gmail.com>
2014-04-15 08:30:54 +0600

    basic function added

--- hiren.js
+++ hiren.js
@@ -1,1 +1,43 @@
 #!/usr/bin/env node
+var Table = require('cli-table') ,
+    request = require('request');
+
+
+function getMovieInfo() {
+    request({
+        uri: "http://0.0.0.0:5000/cli-movie",
+        method: "POST",
+        form: {
+            'movie': 'valuesaasa', 'year': ''
+        }
+    }, function (error, response, body) {
+        console.log(body);
+    });
+
+}
+
+function cli() {
+
+    var data = process.argv.slice(2);
+    if(data == "--help"){
+        console.log('Usage: movie [Movie Name] [Year]');
+        console.log('Example: movie "Lord Of The Rings"');
+        process.exit(1);
+    }
+    else{
+        if(process.argv.slice(3) = "" ) return { 'movie' : data , 'year' : "" }
+        else return { 'movie' : data , 'year' : process.argv.slice(3) }
+    }
+
+}
+
+
+function table(rating , type , year , genre , movieName) {
+    var table = new Table({ head: [ movieName , 'Rating' , 'Type' , 'Year' , 'Genre'] });
+
+    table.push(
+        { 'IMDB': [rating, type , year , genre] }
+    );
+    console.log(table.toString());
+
+}
