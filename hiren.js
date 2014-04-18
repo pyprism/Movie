c29164b63109f41016e66f79bf7d39117a55d0db
pyprism <git.pyprism@gmail.com>
2014-04-18 23:14:28 +0600

    small fix

--- hiren.js
+++ hiren.js
@@ -10,16 +10,8 @@ function getMovieInfo() {
         form: cli()
     }, function (error, response, body) {
         var json = JSON.parse(body);
-        if(!json['error']) {
-           // console.log(json['Genre']);
-        //console.log(body);
-            table( json['"Awards"'] ,json['imdbRating'] , json['Rated'] , json['Type'], json['Year'] , json['Genre'] ,json['Title']);
-            //console.log(json['Rating'] );
-           // console.log(json['Runtime']);
-           // console.log(json['Year']);
-           // console.log(json['Genre']);
-           // console.log(json['Title']);
-        }
+        if(!json['error'])
+            table( json["Awards"] ,json['imdbRating'] , json['Rated'] , json['Type'], json['Year'] , json['Genre'] ,json['Title']);
         else console.log("Movie you Are searching is not found :( ");
     });
 
@@ -43,7 +35,7 @@ function cli() {
 
 
 function table(awards ,imdbRating , rated , type , year , genre , movieName) {
-    var table = new Table({ head: [ movieName , 'Rating', 'Rated' , 'Type' , 'Year' , 'Genre'] });
+    var table = new Table({ head: [ movieName , 'Awards' ,'Rating', 'Rated' , 'Type' , 'Year' , 'Genre'] });
 
     table.push(
         { 'IMDB': [ awards, imdbRating , rated, type , year , genre] }
