9265b2cadc46d75f9f306673eba77e2b3cb81226
Ashutosh Das <git.pyprism@gmail.com>
2016-11-14 21:14:34 +0600

    added drf swagger

--- hiren/urls.py
+++ hiren/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_swagger.views import get_swagger_view
+
+
+schema_view = get_swagger_view(title='Movie API')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^doc/$', schema_view),
 ]
