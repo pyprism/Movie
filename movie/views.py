0103aa75d27807fe8395e2497e0fd8ea5b3a57dd
Ashutosh Das <git.pyprism@gmail.com>
2016-11-16 19:22:01 +0600

    added new views

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from .serializers import HirenSerializer, MovieSerializer, MovieTypeSerializer, BunnySerializer
+from .models import Hiren, Movie, MovieType
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class MovieViewset(viewsets.ModelViewSet):
+    """
+    API endpoint that allows movie to be created, updated ,viewed and deleted
+    """
+    queryset = Hiren.objects.all()
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (SessionAuthentication, BasicAuthentication, JSONWebTokenAuthentication)
+    serializer_class = HirenSerializer
+
+    def create(self, request, *args, **kwargs):
+        bunny = BunnySerializer(data=self.request.DATA)
+        if bunny.is_valid():
+            x = bunny.save()
+            return Response(x, status.HTTP_201_CREATED)
+        else:
+            return Response('error', status.HTTP_400_BAD_REQUEST)
