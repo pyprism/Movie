721882eb98772e6981bdf77f18c97be973df283c
Ashutosh Das <git.pyprism@gmail.com>
2016-11-16 21:07:44 +0600

    refactored

--- movie/serializers.py
+++ movie/serializers.py
@@ -16,56 +16,9 @@ class HirenSerializer(serializers.ModelSerializer):
         model = Hiren
         fields = ('movie', 'id', 'watched_full', 'rating', 'source', 'video_quality', 'watched_at')
 
+    def create(self, validated_data):
+        movie_data = validated_data.pop('movie')
+        movie = Movie.objects.create(**movie_data)
+        hiren = Hiren.objects.create(movie=movie, **validated_data)
+        return hiren
 
-class BunnySerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=800)
-    imdb_rating = serializers.IntegerField(max_value=10, min_value=0)
-    watched_full = serializers.BooleanField(default=True)
-    rating = serializers.IntegerField(max_value=10, min_value=0)
-    source = serializers.CharField(max_length=500, allow_blank=True)
-    watched_at = serializers.DateField()
-    quality_choice = (
-        ('HD', 'HD'),
-        ('Cam', 'Cam'),
-        ('Dvd', 'DVD'),
-        ('480', '480'),
-        ('720', '720'),
-        ('108', '1080'),
-    )
-    video_quality = serializers.ChoiceField(choices=quality_choice)
-    movie_choice = (
-        ('Act', 'Action'),
-        ('Ani', 'Animation'),
-        ('Com', 'Comedy'),
-        ('Doc', 'Documentary'),
-        ('Fam', 'Family'),
-        ('FN', 'Film-Noir'),
-        ('Hor', 'Horror'),
-        ('Mus', 'Musical'),
-        ('Rom', 'Romance'),
-        ('Spo', 'Sport'),
-        ('War', 'War'),
-        ('Adv', 'Adventure'),
-        ('Bio', 'Biography'),
-        ('Cri', 'Crime'),
-        ('Dra', 'Drama'),
-        ('Fan', 'Fantasy'),
-        ('His', 'History'),
-        ('Mus', 'Music'),
-        ('Mys', 'Mystery'),
-        ('Sci', 'Sci-Fi'),
-        ('Thr', 'Thriller'),
-        ('Wes', 'Western'),
-    )
-    movie_type = serializers.ChoiceField(choices=movie_choice)
-
-    def save(self):
-        movie = Movie.objects.create(name=self.validated_data['name'],
-                                     imdb_rating=self.validated_data['imdb_rating'],
-                                     movie_type=self.validated_data['movie_type'])
-        Hiren.objects.create(movie=movie,
-                             watched_full=self.validated_data['watched_full'],
-                             rating=self.validated_data['rating'],
-                             source=self.validated_data['source'],
-                             watched_at=self.validated_data['watched_at'],
-                             video_quality=self.validated_data['video_quality'])
