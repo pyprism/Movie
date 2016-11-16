f542e0b04615feb964d00d90bc348af230b2b67e
Ashutosh Das <git.pyprism@gmail.com>
2016-11-16 19:56:27 +0600

    refactored model

--- movie/models.py
+++ movie/models.py
@@ -4,31 +4,6 @@ from django.db import models
 class Movie(models.Model):
     name = models.CharField(max_length=800, unique=True)
     imdb_rating = models.IntegerField(null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-class Hiren(models.Model):
-    movie = models.ForeignKey(Movie)
-    watched_full = models.BooleanField(default=True)
-    rating = models.IntegerField()
-    source = models.CharField(max_length=500, null=True)
-    watched_at = models.DateField()
-    quality_choice = (
-        ('HD', 'HD'),
-        ('Cam', 'Cam'),
-        ('Dvd', 'DVD'),
-        ('480', '480'),
-        ('720', '720'),
-        ('108', '1080'),
-    )
-    video_quality = models.CharField(max_length=3, choices=quality_choice)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
-
-
-class MovieType(models.Model):
-    movie = models.ForeignKey(Movie)
     movie_choice = (
         ('Act', 'Action'),
         ('Ani', 'Animation'),
@@ -54,3 +29,24 @@ class MovieType(models.Model):
         ('Wes', 'Western'),
     )
     movie_type = models.CharField(max_length=3, choices=movie_choice)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Hiren(models.Model):
+    movie = models.ForeignKey(Movie)
+    watched_full = models.BooleanField(default=True)
+    rating = models.IntegerField()
+    source = models.CharField(max_length=500, null=True)
+    watched_at = models.DateField()
+    quality_choice = (
+        ('HD', 'HD'),
+        ('Cam', 'Cam'),
+        ('Dvd', 'DVD'),
+        ('480', '480'),
+        ('720', '720'),
+        ('108', '1080'),
+    )
+    video_quality = models.CharField(max_length=3, choices=quality_choice)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
