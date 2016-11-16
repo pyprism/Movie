5ba328221a98ff50fd4f74df314e7a9256c9e8f4
Ashutosh Das <git.pyprism@gmail.com>
2016-11-16 19:22:13 +0600

    added new url

--- hiren/urls.py
+++ hiren/urls.py
@@ -17,13 +17,19 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_swagger.views import get_swagger_view
+from movie import views
+from rest_framework import routers
 
 
 schema_view = get_swagger_view(title='Movie API')
+
+router = routers.DefaultRouter()
+router.register(r'movies', views.MovieViewset)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^doc/$', schema_view),
+    url(r'^api/', include(router.urls)),
 ]
