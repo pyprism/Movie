3668e2de80a729e4428ce66f46d920a2383c9cc5
Ashutosh Das <git.pyprism@gmail.com>
2016-11-17 22:02:30 +0600

    fixied api update

--- movie/serializers.py
+++ movie/serializers.py
@@ -22,3 +22,22 @@ class HirenSerializer(serializers.ModelSerializer):
         hiren = Hiren.objects.create(movie=movie, **validated_data)
         return hiren
 
+    def update(self, instance, validated_data):
+        movie_name = validated_data.get('movie', {}).get('name')
+        print(movie_name)
+        print(instance.movie.name)
+
+        if movie_name != instance.movie.name:
+            instance.movie.name = movie_name
+        # instance.movie.name = validated_data.get('movie', {}).get('name')
+        instance.movie.imdb_rating = validated_data.get('movie', {}).get('imdb_rating')
+        instance.movie.movie_type = validated_data.get('movie', {}).get('movie_type')
+        instance.watched_full = validated_data.get('watched_full', instance.watched_full)
+        instance.rating = validated_data.get('rating', instance.rating)
+        instance.source = validated_data.get('source', instance.source)
+        instance.video_quality = validated_data.get('video_quality', instance.video_quality)
+        instance.watched_at = validated_data.get('watched_at', instance.watched_at)
+        # instance.movie.save()
+        instance.save()
+
+        return instance
