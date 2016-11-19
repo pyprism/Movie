7eb4e550c659ece0728a3602a5ed44b4f2234be4
Ashutosh Das <git.pyprism@gmail.com>
2016-11-19 20:18:02 +0600

    fixed url

--- bunny/components/Login.jsx
+++ bunny/components/Login.jsx
@@ -19,7 +19,7 @@ export default class Login extends React.Component {
         }).then(function (response) {
             if (response.data['token']) {
                 sessionStorage.setItem('token', response.data['token']);
-                browserHistory.push('/dashboard/secret/');
+                browserHistory.push('/dashboard/');
             }
         })
             .catch(function (response) {
