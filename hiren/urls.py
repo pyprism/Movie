a6bfe611e3d3d74c20d8c8b852075b3e53adefdb
Ashutosh Das <git.pyprism@gmail.com>
2016-11-19 20:15:34 +0600

    added jwt check url

--- hiren/urls.py
+++ hiren/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 from rest_framework_swagger.views import get_swagger_view
 from movie import views
 from rest_framework import routers
@@ -31,6 +31,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
     url(r'^doc/$', schema_view),
     url(r'^api/', include(router.urls)),
     url(r'^', TemplateView.as_view(template_name='index.html')),
