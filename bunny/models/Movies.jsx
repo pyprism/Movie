0836f7e79c3d4ca8607df7a9dc3063bfec0aae53
Ashutosh Das <git.pyprism@gmail.com>
2016-11-22 22:38:05 +0600

    added model

---
+++ bunny/models/Movies.jsx
@@ -0,0 +1,26 @@
+import { observable, action, computed } from 'mobx';
+import axios from 'axios';
+
+
+export class Movies {
+	@observable loaded = false;
+	@observable movies = [];
+
+	@action getMovies() {
+		axios({
+			method: 'get',
+			url: '/api/movies/',
+			headers: {'Authorization': "JWT " + sessionStorage.getItem('token')}
+		}).then(action('response action', (response) => {
+			console.log(response.data);
+			this.movies.push(response);
+            this.loaded = true;
+		})).catch(function(err) {
+			sweetAlert("Oops!", err.data, "error");
+		})
+	}
+
+    @computed get movieList() {
+        return this.movies;
+    }
+}
