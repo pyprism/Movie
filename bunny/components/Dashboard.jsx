304dc3abb2caa1787e3baf384b5aafff6a1d23e1
Ashutosh Das <git.pyprism@gmail.com>
2016-11-23 20:21:57 +0600

    WIP added grrrrrrrrr......hiren.......

--- bunny/components/Dashboard.jsx
+++ bunny/components/Dashboard.jsx
@@ -4,26 +4,30 @@ import { observable, computed } from "mobx";
 import { observer } from "mobx-react";
 import { Movies } from '../models/Movies.jsx';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import { Provider } from 'mobx-react';
 
 
 @observer
 export default class Dashboard extends React.Component {
 
+    constructor(props) {
+        super(props);
+    }
+
     componentDidMount(){
-        const movies = new Movies();
-        movies.getMovies();
-        console.log(movies.loaded);
-        console.log(movies.movieList);
+        //const movies = new Movies();
+        //movies.getMovies();
+        //console.log(movies.loaded);
+        //console.log(movies.movieList);
+        console.log(this.props.route.movie.loaded );
     }
 
     render() {
-        if(new Movies().loaded) {
-            return (
+
+        return (
                 <div>
-                    I am dumb dashboard
+               bvcb
                 </div>
-            )
-        }
-        return <div>loading all data .... </div>
+        )
     }
 }
