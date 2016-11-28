9b5cbbb449c2e6464683a081e89d410fa07332d1
Ashutosh Das <git.pyprism@gmail.com>
2016-11-28 19:07:29 +0600

    wip

--- movie/tests.py
+++ movie/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
+from django.utils.timezone import localtime, now
+from rest_framework.test import APIRequestFactory, APIClient
+from django.contrib.auth.models import User
+from .models import Movie, Hiren
+from freezegun import freeze_time
 
-# Create your tests here.
+
+class ModelTest(TransactionTestCase):
+    """
+    Test all models
+    """
+    current_date = localtime(now()).date()
+    reset_sequences = True
+
+    @freeze_time("2012-01-14")
+    def setUp(self):
+        movie = Movie.objects.create(
+            name="Schindler's List",
+            imdb_rating=9,
+            movie_type='His',
+        )
+
+    def test_movie_model(self):
+        mov = Movie.objects.all()
+        print(mov)
