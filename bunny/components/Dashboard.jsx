d98ad3cbc5216b0b10087636706abab191713656
Ashutosh Das <git.pyprism@gmail.com>
2016-12-08 18:41:32 +0600

    wip

--- bunny/components/Dashboard.jsx
+++ bunny/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { observable, computed } from "mobx";
+import { observable, computed, toJS } from "mobx";
 import { observer } from "mobx-react";
 import { Movies } from '../models/Movies.jsx';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
@@ -16,18 +16,49 @@ export default class Dashboard extends React.Component {
 
     componentDidMount(){
         //const movies = new Movies();
-        //movies.getMovies();
+        this.props.route.movie.getMovies();
         //console.log(movies.loaded);
         //console.log(movies.movieList);
-        console.log(this.props.route.movie.loaded );
+        //console.log(this.props.route.movie.loaded);
+        //console.log(toJS(this.props.route.movie.movies));
     }
 
     render() {
 
-        return (
+        if(this.props.route.movie.loaded){
+            console.log(toJS(this.props.route.movie.movies));
+
+            function movieName(data, cell){
+                return cell.movie.name;
+            }
+
+            function movieIMDB(data, cell){
+                return cell.movie.imdb_rating;
+            }
+
+            function movieType(data, cell){
+                return cell.movie.movie_type;
+            }
+
+            return (
                 <div>
-               bvcb
+                <BootstrapTable data={toJS(this.props.route.movie.movieList)} striped={true} hover={true} condensed={true} pagination={true} search={true}>
+                        <TableHeaderColumn dataField="id" isKey={true}>ID</TableHeaderColumn>
+                        <TableHeaderColumn dataField="movie" dataFormat={ movieName} >Name</TableHeaderColumn>
+                        <TableHeaderColumn dataField="movie" dataFormat={ movieIMDB} >IMDB Rating</TableHeaderColumn>
+                        <TableHeaderColumn dataField="movie" dataFormat={ movieType} >Movie Type</TableHeaderColumn>
+                        <TableHeaderColumn dataField="rating" >Rating</TableHeaderColumn>
+                        <TableHeaderColumn dataField="source" >Source</TableHeaderColumn>
+                        <TableHeaderColumn dataField="video_quality">Quality</TableHeaderColumn>
+                        <TableHeaderColumn dataField="watched_at">Watched At</TableHeaderColumn>
+                        <TableHeaderColumn dataField="watched_full"> Watched Full</TableHeaderColumn>
+                </BootstrapTable>
                 </div>
         )
+        }
+        return (
+            <div>Loading........</div>
+        )
+
     }
 }
