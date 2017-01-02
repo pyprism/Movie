8498d857bada1f3a33fe76fb37f765ca55cf668c
Ashutosh Das <git.pyprism@gmail.com>
2017-01-02 23:54:14 +0600

    updated form

--- bunny/components/Movie.jsx
+++ bunny/components/Movie.jsx
@@ -3,46 +3,73 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 export default class Movie extends React.Component {
-    
-    componentDidMount() {
-        
+
+    form(e) {
+        e.preventDefault();
+        axios({
+            method: 'post',
+            url: '/api/movies/',
+            data: {
+                'movie' : {
+                    'name': ReactDOM.findDOMNode(this.refs.name).value,
+                    'imdb_rating': ReactDOM.findDOMNode(this.refs.imdb_rating).value,
+                    'movie_type': ReactDOM.findDOMNode(this.refs.movie_type).value
+                },
+                'rating': ReactDOM.findDOMNode(this.refs.rating).value,
+                'source': ReactDOM.findDOMNode(this.refs.source).value,
+                'video_quality': ReactDOM.findDOMNode(this.refs.video_quality).value,
+                'watched_at': ReactDOM.findDOMNode(this.refs.watched_at).value,
+                'watched_full': ReactDOM.findDOMNode(this.refs.watched_full).value
+            },
+            headers: {
+                'Authorization': "JWT " + sessionStorage.getItem('token')
+            }
+        }).then(function (response) {
+            console.log(response);
+        }).catch(function (error) {
+            console.log(error);
+        })
     }
-    
+
+    componentDidMount() {
+
+    }
+
     render (){
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-sm-6 col-sm-offset-2">
-                        <form action="" className="form-horizontal pad-bg">
+                        <form className="form-horizontal pad-bg" onSubmit={this.form.bind(this)}>
                             <h1 className="text-center">Save New Movie</h1>
                             <div className="form-group">
                                 <label className="control-label col-sm-3">Name</label>
                                 <div className="col-sm-9">
-                                    <input type="text" name="name" required className="form-control input-lg" />
+                                    <input type="text" ref="name" required placeholder="Name of the movie" className="form-control input-lg" />
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label className="control-label col-sm-3">IMDB Rating</label>
                                 <div className="col-sm-9">
-                                    <input type="number" min="1" max="10" name="imdb_rating" className="form-control input-lg" />
+                                    <input type="number" min="1" max="10" ref="imdb_rating" placeholder="Current IMDB rating" className="form-control input-lg" />
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label className="control-label col-sm-3">Rating</label>
                                 <div className="col-sm-9">
-                                    <input type="number" min="1" max="10" name="rating" required className="form-control input-lg" />
+                                    <input type="number" min="1" max="10" ref="rating" placeholder="Rating of yours" required className="form-control input-lg" />
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label className="control-label col-sm-3">Source</label>
                                 <div className="col-sm-9">
-                                    <input type="text" required name="source" className="form-control input-lg" />
+                                    <input type="text" required placeholder="Where did you watched" ref="source" className="form-control input-lg" />
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label className="control-label col-sm-3">Video Quality</label>
                                 <div className="col-sm-9">
-                                    <select className="form-control input-lg" required name="video_quality">
+                                    <select className="form-control input-lg" required ref="video_quality">
                                         <option value="HD">HD</option>
                                         <option value="Cam">Cam</option>
                                         <option value="Dvd">DVD</option>
@@ -55,7 +82,7 @@ export default class Movie extends React.Component {
                             <div className="form-group">
                                 <label className="control-label col-sm-3">Movie Type</label>
                                 <div className="col-sm-9">
-                                    <select className="form-control input-lg" required name="movie_type">
+                                    <select className="form-control input-lg" required ref="movie_type">
                                         <option value="Act">Action</option>
                                         <option value="Ani">Animation</option>
                                         <option value="Com">Comedy</option>
@@ -84,21 +111,21 @@ export default class Movie extends React.Component {
                             <div className="form-group">
                                 <label className="control-label col-sm-3">Watched At</label>
                                 <div className="col-sm-9">
-                                    <input  data-provide="datepicker" data-date-format="dd/mm/yyyy" required name="imdb_rating" className="form-control input-lg" />
+                                    <input  data-provide="datepicker"  placeholder="Date" data-date-format="dd-mm-yyyy" required ref="watched_at" className="form-control input-lg" />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label className="control-label col-sm-3">Watched Full</label>
+                                <div className="col-sm-9">
+                                    <select className="form-control input-lg" ref="watched_full">
+                                        <option value="True"> Yes </option>
+                                        <option value="False"> No</option>
+                                    </select>
                                 </div>
                             </div>
                             <div className="form-group">
                                 <div className="col-sm-offset-3 col-sm-9">
-                                    <div className="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="watched_full" /> Watched Full
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="form-group">
-                                <div className="col-sm-offset-3 col-sm-9">
-                                    <button className="btn btn-default btn-lg"><i className="fa fa-save"></i> Save</button>
+                                    <button type="submit" className="btn btn-default btn-lg"><i className="fa fa-save"></i> Save</button>
                                 </div>
                             </div>
                         </form>
