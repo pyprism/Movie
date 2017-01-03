75aa1d2df096f58b35459f87c0b086aa66947b23
Ashutosh Das <git.pyprism@gmail.com>
2017-01-03 20:09:33 +0600

    Added formm

--- bunny/components/Dashboard.jsx
+++ bunny/components/Dashboard.jsx
@@ -10,9 +10,9 @@ import { Provider } from 'mobx-react';
 @observer
 export default class Dashboard extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+    //constructor(props) {
+    //    super(props);
+    //}
 
     componentDidMount(){
         //const movies = new Movies();
@@ -26,7 +26,6 @@ export default class Dashboard extends React.Component {
     render() {
 
         if(this.props.route.movie.loaded){
-            console.log(toJS(this.props.route.movie.movies));
 
             function movieName(data, cell){
                 return cell.movie.name;
@@ -37,24 +36,82 @@ export default class Dashboard extends React.Component {
             }
 
             function movieType(data, cell){
-                return cell.movie.movie_type;
+                if (data.movie_type == "Act")
+                    return "Action";
+                else if(data.movie_type = "Ani")
+                    return "Animation";
+                else if(data.movie_type = "Com")
+                    return "Comedy";
+                else if(data.movie_type = "Doc")
+                    return "Documentary";
+                else if(data.movie_type = "Fam")
+                    return "Family";
+                else if(data.movie_type = "FN")
+                    return "Film-Noir";
+                else if(data.movie_type = "Hor")
+                    return "Horror";
+                else if(data.movie_type = "Mus")
+                    return "Musical";
+                else if(data.movie_type = "Rom")
+                    return "Romance";
+                else if(data.movie_type = "Spo")
+                    return "Sport";
+                else if(data.movie_type = "War")
+                    return "War";
+                else if(data.movie_type = "Adv")
+                    return "Adventure";
+                else if(data.movie_type = "Bio")
+                    return "Biography";
+                else if(data.movie_type = "Cri")
+                    return "Crime";
+                else if(data.movie_type = "Dra")
+                    return "Drama";
+                else if(data.movie_type = "Fan")
+                    return "Fantasy";
+                else if(data.movie_type = "His")
+                    return "History";
+                else if(data.movie_type = "Mus")
+                    return "Music";
+                else if(data.movie_type = "Mys")
+                    return "Mystery";
+                else if(data.movie_type = "Sci")
+                    return "Sci-Fi";
+                else if(data.movie_type = "Thr")
+                    return "Thriller";
+                else if(data.movie_type = "Wes")
+                    return "Western";
+                return "Undefined";
+            }
+
+            function movieWatchedFull(data, cell) {
+                if(data)
+                    return 'Yes';
+                else if(!data)
+                    return 'No';
+                return 'Undefined';
+            }
+
+            function filterName(cell, row) {
+                console.log(cell);
+                console.log(row);
+                return cell.movie.name;
             }
 
             return (
                 <div>
-                <BootstrapTable data={toJS(this.props.route.movie.movieList)} striped={true} hover={true} condensed={true} pagination={true} search={true}>
+                    <BootstrapTable data={toJS(this.props.route.movie.movieList)} striped={true} hover={true} condensed={true} pagination={true} search={true}>
                         <TableHeaderColumn dataField="id" isKey={true}>ID</TableHeaderColumn>
-                        <TableHeaderColumn dataField="movie" dataFormat={ movieName} >Name</TableHeaderColumn>
+                        <TableHeaderColumn dataField="movie" filterValue={filterName} dataFormat={ movieName} >Name</TableHeaderColumn>
                         <TableHeaderColumn dataField="movie" dataFormat={ movieIMDB} >IMDB Rating</TableHeaderColumn>
                         <TableHeaderColumn dataField="movie" dataFormat={ movieType} >Movie Type</TableHeaderColumn>
                         <TableHeaderColumn dataField="rating" >Rating</TableHeaderColumn>
                         <TableHeaderColumn dataField="source" >Source</TableHeaderColumn>
                         <TableHeaderColumn dataField="video_quality">Quality</TableHeaderColumn>
                         <TableHeaderColumn dataField="watched_at">Watched At</TableHeaderColumn>
-                        <TableHeaderColumn dataField="watched_full"> Watched Full</TableHeaderColumn>
-                </BootstrapTable>
+                        <TableHeaderColumn dataField="watched_full" dataFormat={movieWatchedFull}> Watched Full</TableHeaderColumn>
+                    </BootstrapTable>
                 </div>
-        )
+            )
         }
         return (
             <div>Loading........</div>
