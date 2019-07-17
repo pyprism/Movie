29eb2376d58dccf063eec3e75bdcaba14f7e893a
Ashutosh Das <git.pyprism@gmail.com>
2019-07-17 21:48:48 +0600

    removed routes

--- hiren/urls.py
+++ hiren/urls.py
@@ -14,26 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
-from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
-from rest_framework_swagger.views import get_swagger_view
 from movie import views
 from rest_framework import routers
 from django.views.generic import TemplateView
-
-
-schema_view = get_swagger_view(title='Movie API')
 
 router = routers.DefaultRouter()
 router.register(r'movies', views.MovieViewset)
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api-token-auth/', obtain_jwt_token),
-    url(r'^api-token-verify/', verify_jwt_token),
-    url(r'^doc/$', schema_view),
     url(r'^api/movielist/$', views.MovieListView.as_view()),
     url(r'^api/', include(router.urls)),
-    url(r'^', TemplateView.as_view(template_name='index.html')),
+   # url(r'^', TemplateView.as_view(template_name='index.html')),
 ]
