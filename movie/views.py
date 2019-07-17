ab31229e6aec9fe1f9293baffea5c7d1cc604d33
Ashutosh Das <git.pyprism@gmail.com>
2019-07-17 21:48:58 +0600

    fixed views

--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from .serializers import HirenSerializer, MovieSerializer
@@ -16,7 +15,7 @@ class MovieListView(ListAPIView):
     """
     queryset = Movie.objects.all()
     permission_classes = (IsAuthenticated,)
-    authentication_classes = (SessionAuthentication, BasicAuthentication, JSONWebTokenAuthentication)
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
     serializer_class = MovieSerializer
 
 
@@ -26,7 +25,7 @@ class MovieViewset(viewsets.ModelViewSet):
     """
     queryset = Hiren.objects.all()
     permission_classes = (IsAuthenticated,)
-    authentication_classes = (SessionAuthentication, BasicAuthentication, JSONWebTokenAuthentication)
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
     serializer_class = HirenSerializer
 
     def list(self, request, *args, **kwargs):
